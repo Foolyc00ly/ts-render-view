78444f783c08d24437c9a8624ce7af8fc068b9e8
Foolyc00ly <63631238+Foolyc00ly@users.noreply.github.com>
2023-10-23 02:11:18 -0500

    cumpliendo mi capricho 6

--- src/components/My-View-App-Root/app/My-Component-Root.class.ts
+++ src/components/My-View-App-Root/app/My-Component-Root.class.ts
@@ -1,8 +1,11 @@
 import { html, render } from 'lit-html';
 import{AppRoot} from '../My-App-Root.class';
+import {  fromEvent } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import style from'./sass/style.scss?inline';
 export class ComponentRoot{
    private component:AppRoot;
+   private baseUrl = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
    constructor(component: AppRoot){
       this.component=component;
    }
@@ -12,6 +15,7 @@ export class ComponentRoot{
       (this.component.observer as MutationObserver).observe(this.component.container as HTMLDivElement,{characterData:true,childList:true,subtree:true});
       this._renderAppShadow();
       this._getNavLinks();
+
    }
    private _renderAppShadow(){
       this.component.renderView('');
@@ -26,7 +30,29 @@ export class ComponentRoot{
    private _getNavLinks(){
       this.component.querySelectorAll('.menu__item').forEach((menuItem)=>{
          const view=(menuItem.textContent?.trim() || '').toLowerCase();
-         globalThis.addEventListener(view,()=>{this.component.renderView(view)})
+         fromEvent<CustomEvent<{view:string}>>(globalThis, view)
+         .pipe(
+            map((event: Event) => event.target as HTMLElement),
+         ).subscribe(() => {
+               this.component.renderView(view);
+               window.history.pushState({ view }, '', `${this.baseUrl}/${view}`);
+         });
+      });
+      fromEvent<PopStateEvent>(window, 'popstate')
+      .pipe(map((event) => event.state?.view || 'home'),startWith(()=>{
+         const path = window.location.pathname;
+         const view = path === '/' ? 'home' : path.substring(1);
+         window.history.pushState({ view }, '', `/${view}`);
+         console.log(view)
+      }))
+      .subscribe((view) => {
+         this.component.renderView(view);
+      });
+      fromEvent(window, 'load').subscribe(()=>{
+         const path = window.location.pathname;
+         const view = path === '/' ? 'home' : path.substring(1);
+         window.history.pushState({ view }, '', `/${view}`);
+         this.component.renderView(view);
       });
    }
 }
